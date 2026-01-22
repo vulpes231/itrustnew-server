7674e2f985e2b638488cf079ad5dfd236ec463bb
Vulpes231 <whykayshot@gmail.com>
2026-01-22 17:49:47 +0100

    init

--- configs/settings.js
+++ configs/settings.js
@@ -2,32 +2,33 @@ const { mongoose } = require("mongoose");
 require("dotenv").config();
 
 const allowedOrigins = [
-	"http://localhost:5173",
-	"https://itrust-admin.netlify.app",
-	"https://itrustinvestment.netlify.app",
+  "http://localhost:5173",
+  "https://itrust-admin.netlify.app",
+  "https://itrustinvestment.netlify.app",
 ];
+
 const DATABASE_URI = process.env.DATABASE_URI;
 
 async function connectDB() {
-	try {
-		await mongoose.connect(DATABASE_URI);
-		console.log("Database connection established.");
-	} catch (error) {
-		console.error("❌ Database connection failed:", error.message);
-		process.exit(1);
-	}
+  try {
+    await mongoose.connect(DATABASE_URI);
+    console.log("Database connection established.");
+  } catch (error) {
+    console.error("❌ Database connection failed:", error.message);
+    process.exit(1);
+  }
 }
 
 const corsOptions = {
-	origin: (origin, callback) => {
-		if (allowedOrigins.includes(origin) || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-		}
-	},
-	credentials: true,
-	optionsSuccessStatus: 200,
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 module.exports = { connectDB, corsOptions };
--- handlers/admin/manageTransactionController.js
+++ handlers/admin/manageTransactionController.js
@@ -1,86 +1,86 @@
 const {
-	fetchAllTransactions,
-	getTransactionInfo,
-	editTransaction,
-	createTransaction,
+  fetchAllTransactions,
+  getTransactionInfo,
+  editTransaction,
+  createTransaction,
 } = require("../../services/admin/manageTransactionService");
 
 const getTransactionData = async (req, res, next) => {
-	const { transactionId } = req.params;
-	try {
-		const transaction = await getTransactionInfo(transactionId);
-		res.status(200).json({
-			data: transaction,
-			success: true,
-			message: "transaction data fetched successfully",
-		});
-	} catch (error) {
-		next(error);
-	}
+  const { transactionId } = req.params;
+  try {
+    const transaction = await getTransactionInfo(transactionId);
+    res.status(200).json({
+      data: transaction,
+      success: true,
+      message: "transaction data fetched successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getAllTransactions = async (req, res, next) => {
-	const sortBy = req.query.sortBy;
-	const limit = Math.min(req.query.limit || 15);
-	const page = Math.max(req.query.page || 1);
-	const filterBy = req.query.filterBy;
-	try {
-		const { transactions, totalTrnxs, totalPages, currentPage } =
-			await fetchAllTransactions({
-				sortBy,
-				limit,
-				page,
-				filterBy,
-			});
-		res.status(200).json({
-			data: transactions,
-			success: true,
-			message: "transactions fetched successfully",
-			pagination: {
-				totalPage: totalPages,
-				totalItem: totalTrnxs,
-				currentPage: currentPage,
-			},
-		});
-	} catch (error) {
-		next(error);
-	}
+  const sortBy = req.query.sortBy;
+  const limit = Math.min(req.query.limit || 15);
+  const page = Math.max(req.query.page || 1);
+  const filterBy = req.query.filterBy;
+  try {
+    const { transactions, totalTrnxs, totalPages, currentPage } =
+      await fetchAllTransactions({
+        sortBy,
+        limit,
+        page,
+        filterBy,
+      });
+    res.status(200).json({
+      data: transactions,
+      success: true,
+      message: "transactions fetched successfully",
+      pagination: {
+        totalPage: totalPages,
+        totalItem: totalTrnxs,
+        currentPage: currentPage,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const updateTransaction = async (req, res, next) => {
-	const { transactionId } = req.params;
-	const { action } = req.body;
+  const { transactionId } = req.params;
+  const { action } = req.body;
 
-	console.log(action, transactionId);
-	try {
-		await editTransaction(transactionId, action);
-		res.status(200).json({
-			data: null,
-			success: true,
-			message: "transaction updated successfully",
-		});
-	} catch (error) {
-		next(error);
-	}
+  console.log(action, transactionId);
+  try {
+    await editTransaction(transactionId, action);
+    res.status(200).json({
+      data: null,
+      success: true,
+      message: "transaction updated successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const adminCreateTransaction = async (req, res, next) => {
-	try {
-		const trnxData = req.body;
-		await createTransaction(trnxData);
-		res.status(200).json({
-			message: `${req.body.type} initiated`,
-			success: true,
-			data: null,
-		});
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const trnxData = req.body;
+    await createTransaction(trnxData);
+    res.status(200).json({
+      message: `${req.body.type} initiated`,
+      success: true,
+      data: null,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-	updateTransaction,
-	getAllTransactions,
-	getTransactionData,
-	adminCreateTransaction,
+  updateTransaction,
+  getAllTransactions,
+  getTransactionData,
+  adminCreateTransaction,
 };
