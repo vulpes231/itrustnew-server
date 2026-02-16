535548b756aa5d01759e2c77a2954bdfa8074881
Vulpes231 <whykayshot@gmail.com>
2026-02-16 15:35:01 +0100

    feat: user config

--- services/admin/manageUserConfigService.js
+++ services/admin/manageUserConfigService.js
@@ -2,6 +2,12 @@ const User = require("../../models/User");
 
 const Usersetting = require("../../models/Usersetting");
 const { CustomError } = require("../../utils/utils");
+const { getCurrencyById } = require("../currencyService");
+const {
+  getCountryById,
+  getStateById,
+  getNationById,
+} = require("../locationService");
 
 async function configureUserLimits(formData) {
   const {
@@ -96,26 +102,101 @@ async function configureWalletAddress(formData) {
 }
 
 async function configureUserInfo(formData) {
-  const {
-    firstname,
-    lastname,
-    email,
-    address,
-    countryId,
-    stateId,
-    city,
-    zipCode,
-    dob,
-    experience,
-    currencyId,
-    employment,
-    nationalityId,
-    userId,
-  } = formData;
   try {
+    const { userId, ...updates } = formData;
+
     const user = await User.findById(userId);
     if (!user) throw new CustomError("User not found!", 404);
+
+    const setObject = {};
+
+    if (updates.firstName) setObject["name.firstName"] = updates.firstName;
+
+    if (updates.lastName) setObject["name.lastName"] = updates.lastName;
+
+    if (updates.email) setObject["credentials.email"] = updates.email;
+
+    if (updates.employment)
+      setObject["professionalInfo.employment"] = updates.employment;
+
+    if (updates.experience)
+      setObject["professionalInfo.experience"] = updates.experience;
+
+    if (updates.address)
+      setObject["contactInfo.address.street"] = updates.address;
+
+    if (updates.city) setObject["contactInfo.address.city"] = updates.city;
+
+    if (updates.zipCode)
+      setObject["contactInfo.address.zipCode"] = updates.zipCode;
+
+    if (updates.dob) {
+      const formattedDob = new Date(updates.dob);
+
+      if (isNaN(formattedDob.getTime())) {
+        throw new CustomError("Invalid date format", 400);
+      }
+
+      setObject["personalDetails.dob"] = formattedDob;
+    }
+
+    if (updates.currencyId) {
+      const currency = await getCurrencyById(updates.currencyId);
+      if (!currency) throw new CustomError("Currency not found!", 404);
+
+      setObject["locationDetails.currency"] = {
+        id: currency._id,
+        name: currency.name,
+        symbol: currency.symbol,
+        sign: currency.sign,
+        rate: currency.rate,
+      };
+    }
+
+    if (updates.countryId) {
+      const country = await getCountryById(updates.countryId);
+      if (!country) throw new CustomError("Country not found!", 404);
+
+      setObject["locationDetails.country"] = {
+        countryId: country._id,
+        name: country.name,
+        phoneCode: country.phoneCode,
+      };
+    }
+
+    if (updates.stateId) {
+      const state = await getStateById(updates.stateId);
+      if (!state) throw new CustomError("State not found!", 404);
+
+      setObject["locationDetails.state"] = {
+        stateId: state._id,
+        name: state.name,
+      };
+    }
+
+    if (updates.nationalityId) {
+      const nation = await getNationById(updates.nationalityId);
+      if (!nation) throw new CustomError("Nationality not found!", 404);
+
+      setObject["locationDetails.nationality"] = {
+        id: nation._id,
+        name: nation.name,
+      };
+    }
+
+    if (Object.keys(setObject).length === 0) {
+      return user;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: setObject },
+      { new: true }
+    );
+
+    return updatedUser;
   } catch (error) {
+    if (error instanceof CustomError) throw error;
     throw new CustomError(error.message, 500);
   }
 }
--- services/currencyService.js
+++ services/currencyService.js
@@ -2,25 +2,25 @@ const Currency = require("../models/Currency");
 const { CustomError } = require("../utils/utils");
 
 async function getCurrencies() {
-	try {
-		const currencies = await Currency.find().lean();
-		return currencies;
-	} catch (error) {
-		throw new CustomError("Failed to fetch currencies", 500);
-	}
+  try {
+    const currencies = await Currency.find().lean();
+    return currencies;
+  } catch (error) {
+    throw new CustomError("Failed to fetch currencies", 500);
+  }
 }
 
 async function getCurrencyById(currencyId) {
-	if (!currencyId) {
-		throw new CustomError("Currency ID required!", 400);
-	}
-	try {
-		const currency = await Currency.findById(currencyId);
-		if (!currency) throw new Error("Currency not found!", { statusCode: 404 });
-		return currency;
-	} catch (error) {
-		throw new CustomError("Failed to fetch currency info", 500);
-	}
+  if (!currencyId) {
+    throw new CustomError("Currency ID required!", 400);
+  }
+  try {
+    const currency = await Currency.findById(currencyId);
+    if (!currency) throw new Error("Currency not found!", { statusCode: 404 });
+    return currency;
+  } catch (error) {
+    throw new CustomError("Failed to fetch currency info", 500);
+  }
 }
 
 module.exports = { getCurrencies, getCurrencyById };
