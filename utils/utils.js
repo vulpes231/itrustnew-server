4c9b9b40132691e64838f227a101f8ca986d85a5
adebayo olayinka <whykayshot@gmail.com>
2026-07-19 13:01:51 +0100

    fix limit

--- utils/utils.js
+++ utils/utils.js
@@ -11,7 +11,7 @@ const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp"];
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024,
+    fileSize: 10 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
